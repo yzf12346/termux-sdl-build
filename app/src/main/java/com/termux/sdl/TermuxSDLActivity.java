60d4e67f52d5f86378d7295ab2db4a7f55cafe0e
Lzhiyong <975434530@qq.com>
2020-03-16 08:15:36 +0800

    update app

--- app/src/main/java/com/termux/sdl/TermuxSDLActivity.java
+++ app/src/main/java/com/termux/sdl/TermuxSDLActivity.java
@@ -1,20 +1,46 @@
 package com.termux.sdl;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import org.libsdl.app.SDLActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import org.libsdl.app.SDLActivity;
+import android.Manifest;
 
 public class TermuxSDLActivity extends SDLActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
+        
+        String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        
+        if(!hasPermission(permission)){
+            requestPermission(permission);
+        }
     }
 
+    
+    
+    public boolean hasPermission(String permission){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            return checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
+        else
+            return true;
+    }
+
+    public void requestPermission(String permission){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(shouldShowRequestPermissionRationale(permission)){
+                Toast.makeText(this, "request read sdcard permmission", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{permission},0);
+        }
+    }
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_action, menu);
